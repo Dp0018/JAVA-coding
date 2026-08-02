c945524d729de6c09a4008774656f35dea0d4e95
Darshan Patil <patildarshan004@gmail.com>
2026-08-02 13:25:33 +0530

    Update GitHub profile README

---
+++ LinkedList/intersectionofll.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    def getIntersectionNode(self, headA, headB):
+
+        p1 = headA
+        p2 = headB
+
+        while p1 != p2:
+
+            p1 = headB if p1 is None else p1.next
+
+            p2 = headA if p2 is None else p2.next
+
+        return p1
