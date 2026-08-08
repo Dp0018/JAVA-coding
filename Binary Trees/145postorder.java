e9fde368174f153cc39cc93133eb6581578d6ebd
Darshan Patil <patildarshan004@gmail.com>
2026-08-08 16:39:33 +0530

    Update GitHub profile README

---
+++ Binary Trees/145postorder.java
@@ -0,0 +1,67 @@
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+
+        List<Integer> result = new ArrayList<>();
+
+        postorder(root, result);
+
+        return result;
+    }
+
+    private void postorder(TreeNode root, List<Integer> result) {
+
+        if (root == null) {
+            return;
+        }
+
+        
+        postorder(root.left, result);
+
+        
+        postorder(root.right, result);
+
+        
+        result.add(root.val);
+        
+    }
+}
+
+
+class Solution {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack1 = new Stack<>();
+        Stack<TreeNode> stack2 = new Stack<>();
+
+        stack1.push(root);
+
+        while (!stack1.isEmpty()) {
+
+            TreeNode node = stack1.pop();
+
+            stack2.push(node);
+
+            if (node.left != null) {
+                stack1.push(node.left);
+            }
+
+            if (node.right != null) {
+                stack1.push(node.right);
+            }
+        }
+
+        while (!stack2.isEmpty()) {
+            result.add(stack2.pop().val);
+        }
+
+        return result;
+    }
+}
