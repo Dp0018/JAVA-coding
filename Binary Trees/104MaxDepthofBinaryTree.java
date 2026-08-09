77f044875ef8a6532502cb1bfe74412bbd383b80
Darshan Patil <patildarshan004@gmail.com>
2026-08-09 12:22:18 +0530

    Update GitHub profile README

---
+++ Binary Trees/104MaxDepthofBinaryTree.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int maxDepth(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
+
+//The depth is the number of nodes along the longest path from the root down to a leaf.
